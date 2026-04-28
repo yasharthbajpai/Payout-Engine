dab3e314bac7e0760f551276179fdc62efef390c
Yasharth <lolatolopa@Yasharths-MacBook-Air.local>
2026-04-28 12:24:52 +0530

    pin express 4 for wildcard route support

--- frontend/server.js
+++ frontend/server.js
@@ -15,7 +15,7 @@ app.use('/api', createProxyMiddleware({
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/{*path}', (_req, res) => {
+app.get('*', (_req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
